9dfe43853f9361d5f4bb0d0f092fb3857a41ca7b
Lucian Jones <lucianj@Lucians-MacBook-Pro.local>
2018-04-23 20:18:07 +1200

    alternate buffer function

--- init.el
+++ init.el
@@ -57,7 +57,27 @@ there's no active region."
     (if (region-active-p)
         (setq beg (region-beginning) end (region-end))
       (setq beg (line-beginning-position) end (line-end-position)))
-	(comment-or-uncomment-region beg end)))
+    (comment-or-uncomment-region beg end)))
+
+(require 'seq)
+
+(defun lj-alternate-buffer (&optional window)
+  "Toggle back and forth between two buffers.
+WINDOW sets the window in which to toggle, and defaults to the
+current window."
+  (interactive)
+  (let ((current-buffer (window-buffer window))
+        (buffer-predicate (frame-parameter (window-frame window) 'buffer-predicate)))
+    ;; switch to first buffer previously shown in this window that matches
+    ;; frame-parameter `buffer-predicate'
+    (switch-to-buffer
+     (or (car (seq-filter (lambda (buffer)
+                            (and (not (eq buffer current-buffer))
+                                 (or (null buffer-predicate) (funcall buffer-predicate buffer))))
+                          (seq-map #'car (window-prev-buffers window))))
+         ;; `other-buffer' honors `buffer-predicate' so no need to filter
+         (other-buffer current-buffer t)))))
+
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
@@ -147,11 +167,11 @@ there's no active region."
 
 (lj-leader-def
   "/"   '(counsel-ag :which-key "ag")
-  "TAB" '(ivy-switch-buffer :which-key "prev buffer")
+  "TAB" '(lj-alternate-buffer :which-key "alternate buffer")
+  "bb"  '(ivy-switch-buffer :which-key "prev buffer")
   "SPC" '(counsel-M-x :which-key "M-x")
   ";"   '(lj-comment-or-uncomment-region-or-line :which-key "comment")
   "ff"  '(counsel-find-file :which-key "find file"))
-
 
 (general-create-definer lj-local-leader-def
   :prefix "SPC m")
