d3f87bc555a60ed5849ec5d00b150762b9cd2339
Lucian Jones <lucianm.jones@gmail.com>
2019-08-19 15:29:25 +1200

    fix exec path from shell PATH variable

--- init.el
+++ init.el
@@ -328,11 +328,9 @@
 
 (use-package exec-path-from-shell
   :ensure t
-  :config
-  (exec-path-from-shell-initialize)
+  :init
   (exec-path-from-shell-copy-env "GOPATH")
-  (exec-path-from-shell-copy-env "JAVA_HOME")
-  (exec-path-from-shell-copy-env "PATH"))
+  (exec-path-from-shell-initialize))
 
 ;; Mac OSX specific settings
 (if (eq system-type 'darwin)
